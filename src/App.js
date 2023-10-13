85fcc178d99b512e7234b9429991da5e30ade352
Twinkle <twinklekamra26@gmail.com>
2023-10-13 11:16:59 +0530

    done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Register from './components/LoginScreen/Register';
 import ForgetPasswordForm from './components/ForgetPassword/ForgetPasswordForm';
 import NewPasswordForm from './components/NewPasswordScreen/NewPasswordForm';
 import Dashboard from './components/Dashboard/Dashboard';
+import User from './components/Dashboard/User';
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
         <Route path="/forgotPassword" component={ForgetPasswordForm} />
         <Route path="/newPassword" component={NewPasswordForm} />
         <Route path="/Dashboard" component={Dashboard} />
+        <Route path="/user" component={User} />
+
         {/* Add more routes here */}
       </Switch>
     </Router>
